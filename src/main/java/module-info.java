002332fb1a4d4da52365a358c3fa91e25c97f560
LeGreentea <dero.ivano@gmail.com>
2024-06-16 23:33:28 +0700

    initial commit 2

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module tech.deroivano.pbouasmaven {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens tech.deroivano.pbouasmaven to javafx.fxml;
+    exports tech.deroivano.pbouasmaven;
+}
---
+++ src/main/java/tech/deroivano/pbouasmaven/HelloController.java
@@ -0,0 +1,14 @@
+package tech.deroivano.pbouasmaven;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
